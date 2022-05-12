ba2ea91e21f98bd72c34309eb3b2f2b054d42bcb
KushalTanna24 <202012063@daiict.ac.in>
2022-05-12 17:45:50 +0530

    Added filter and lifted state from child to parent component using controlled components.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,32 +1,47 @@
+import { useState } from "react";
 import Card from "./Card";
 import "./Expenses.css";
 import ExpenseTacker from "./ExpenseTacker";
+import ExpensesFilter from "./Filter/ExpensesFilter";
 
 const Expense = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2020");
+
+  const yearFilterHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+    console.log(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseTacker
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseTacker
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseTacker
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseTacker
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          onFilterHandler={yearFilterHandler}
+          selected={selectedYear}
+        />
+        <ExpenseTacker
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseTacker
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseTacker
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseTacker
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expense;
